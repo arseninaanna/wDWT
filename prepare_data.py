518e57c082dd83e6055b1e80e0437969bc413539
Anna <arseninaanna@mail.ru>
2020-04-09 23:00:20 +0300

    doc2vec

--- prepare_data.py
+++ prepare_data.py
@@ -14,37 +14,30 @@ def get_documents(directory):
     return documents
 
 
-def preprocess(directory, vecor_size = 15):
+def doc2vec(documents, vector_size=15):
     vectors = []
     all_pargraphs = []
     doc_to_paragraphs = {}
     counter = 0
-    for filename in os.listdir(directory):
-        if filename.endswith(".txt"):
-            f = open(directory + "/" + filename, encoding="utf-8-sig")
-            lines = f.read()
-            paragraphs = lines.split('\n\n')
 
-            doc_to_paragraphs[counter] = len(paragraphs)
-            counter += 1
-            for par in paragraphs:
-                all_pargraphs.append(par.lower())
+    for document in documents:
+        paragraphs = document.split('\n\n')
 
-            continue
-        else:
-            continue
+        doc_to_paragraphs[counter] = len(paragraphs)
+        counter += 1
+        for par in paragraphs:
+            all_pargraphs.append(par.lower())
 
     documents = [TaggedDocument(doc, [i]) for i, doc in enumerate(all_pargraphs)]
-    model = Doc2Vec(documents, vector_size=vecor_size, window=2, min_count=1, workers=4)
-    print(model.docvecs[0])
+    model = Doc2Vec(documents, vector_size=vector_size, window=2, min_count=1, workers=4)
 
     start = 0
     for index in doc_to_paragraphs:
         length = doc_to_paragraphs[index]
         doc_vectors = []
-        for i in range(start, length+start):
+        for i in range(start, length + start):
             doc_vectors.append(model.docvecs[i])
         vectors.append(doc_vectors)
-        start = start+length
+        start = start + length
 
     return np.array(vectors)
